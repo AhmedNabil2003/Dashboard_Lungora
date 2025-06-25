7d11e2b2501a4b0cb436c67725c8a1d57a8b97ec
Ahmed Nabil <ahmednabilsayed079@gmail.com>
2025-06-25 23:26:27 +0300

    fix: correct casing for InputField component

--- src/components/ui/inputField.jsx
+++ src/components/ui/inputField.jsx
@@ -3,7 +3,7 @@ const Input = ({ type = "text", placeholder, ...props }) => {
     <input
       type={type}
       placeholder={placeholder}
-      className="w-full border border-gray-300 p-2 rounded-md mt-4 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+      className="w-full border border-gray-300 p-3 rounded-md mt-4 focus:ring-2 focus:ring-blue-500 focus:outline-none"
       {...props}
     />
   );
