2368c42f0c457533e4898df7edada04c22a79fe6
Ahmed Nabil <ahmednabilsayed079@gmail.com>
2025-06-25 22:26:33 +0300

    rename: temp rename for casing correction

---
+++ src/context/auth-context-temp.jsx
@@ -0,0 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  clearAuthStorage,
+  getTokenFromStorage,
+  isRememberMe,
+  storeToken,
+} from "../hooks/useLocalStorage";
+import { logoutSingle } from "../services/apiAuth";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const storedToken = getTokenFromStorage("token"); 
+    if (storedToken) {
+      setToken(storedToken); 
+    }
+    setIsLoading(false);
+  }, []);
+
+  const logout = async () => {
+    try {
+      await logoutSingle(); 
+    } catch (err) {
+      console.error("❌ فشل أثناء محاولة تسجيل الخروج من السيرفر:", err);
+    } finally {
+      clearAuthStorage();
+      setToken(null); 
+      localStorage.removeItem("isAuthenticated");
+    }
+  };
+
+  const updateToken = (newToken) => {
+    storeToken(newToken, isRememberMe());
+    setToken(newToken); 
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, isLoading,setToken, logout, updateToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
